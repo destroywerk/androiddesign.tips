b92e48caa691e871f5869d5317552177cf9089bc
Ben Scott <ben@reload.me.uk>
2014-05-04 16:01:48 +0100

    Add unit calculator

---
+++ js/main.js
@@ -0,0 +1,34 @@
+(function(document){
+
+  var inputName = 'unit-input__input';
+  var inputBaseClass = inputName + '--base';
+
+  var inputSelector =  '.' + inputName;
+  var $baseForm = $('.unit-inputs');
+  var $inputs = $(inputSelector);
+  var multiplierLookup = {};
+
+  $('.unit-input__input').each(function() {
+    multiplierLookup[this.id] = this.getAttribute('data-multiplier');
+  });
+
+
+  $baseForm.on('keyup', inputSelector, function(e) {
+    var baseValue = parseInt(this.value, 10);
+    var baseMultiplier = multiplierLookup[this.id];
+
+    $(this).addClass(inputBaseClass);
+
+    $inputs.not(e.target).each(function(){
+      var currentMultiplier = multiplierLookup[this.id];
+      var value = baseValue * (currentMultiplier / baseMultiplier);
+      var valueString = +value.toFixed(3);
+
+      $(this).removeClass(inputBaseClass);
+
+      // Null out values if the inputed value is 0
+      this.value = (baseValue !== 0 ? valueString : '');
+    });
+
+  });
+}(document));
